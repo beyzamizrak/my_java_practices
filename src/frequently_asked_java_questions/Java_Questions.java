7fa59333dd408edc62875ed4b8d450851b237fa6
Beyza Mizrak <pskmizrak@gmail.com>
2023-01-17 00:19:43 -0600

    added

--- src/frequently_asked_java_questions/Java_Questions.java
+++ src/frequently_asked_java_questions/Java_Questions.java
@@ -1,10 +1,31 @@
 package frequently_asked_java_questions;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Java_Questions {
     public static void main(String[] args) {
 
-        //1. How to reverse a
+        System.out.println("----------------How to reverse a string-----------------");
 
+        String str = "Beyza";
+        String reverse = "";
+        for (int i = str.length()-1; i >= 0 ; i--) {
+            reverse+= str.charAt(i);
+        }
+        System.out.println(reverse);
+
+        System.out.println("----------------How to reverse an array-----------------");
+
+        int[] numbers = {1, 2, 3, 4, 10, 22, 35};
+        ArrayList<Integer> reversedNumbers = new ArrayList<>();
+
+        for (int i = numbers.length-1; i >=0 ; i--) {
+            reversedNumbers.add(numbers[i]);
+        }
+        System.out.println(Arrays.toString(reversedNumbers.toArray()));
+
+        System.out.println("----------------How to -----------------");
 
     }
 }
